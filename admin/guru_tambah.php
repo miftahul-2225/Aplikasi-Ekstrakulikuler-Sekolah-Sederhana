b5214cfdcf69a2749ca68243e9a2c29faf40cff8
Muhammad Miftahul Muhtadin <adinrahman22@gmail.com>
2025-12-31 22:09:56 +0700

    Update guru_tambah..php

--- admin/guru_tambah.php
+++ admin/guru_tambah.php
@@ -90,6 +90,6 @@ if (isset($_POST['simpan'])) {
         </div>
     </div>
 </div>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
