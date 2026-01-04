9663d1e740dad11a8008e955e620065c0ecc6eed
Muhammad Miftahul Muhtadin <adinrahman22@gmail.com>
2026-01-04 21:46:44 +0700

    Update guru_index2.php

--- admin/guru_index2.php
+++ admin/guru_index2.php
@@ -130,16 +130,12 @@ if (!$query) {
                             </td>
                         </tr>
                     <?php endif; ?>
-
                     </tbody>
                 </table>
             </div>
-
         </div>
     </div>
-
 </div>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
