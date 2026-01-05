ad52b441030b3ec606c4488e2b5d2d523c4708f0
Muhammad Miftahul Muhtadin <adinrahman22@gmail.com>
2026-01-05 20:20:36 +0700

    Update siswa_edit.php

--- admin/siswa_edit.php
+++ admin/siswa_edit.php
@@ -34,7 +34,7 @@ if (isset($_POST['update'])) {
     <nav class="navbar navbar-dark bg-primary">
         <div class="container-fluid">
             <span class="navbar-brand fw-bold">
-                <i class="bi bi-person-lines-fill"></i> Edit Data Guru
+                <i class="bi bi-person-lines-fill"></i> Edit Data Siswa
             </span>
         </div>
     </nav>
