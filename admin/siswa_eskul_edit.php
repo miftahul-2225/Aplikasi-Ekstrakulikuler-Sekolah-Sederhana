64c03618cc6c62a08baebed50c9289b4a94d785f
Muhammad Miftahul Muhtadin <adinrahman22@gmail.com>
2026-01-05 20:21:09 +0700

    Update siswa_eskul_edit.php

--- admin/siswa_eskul_edit.php
+++ admin/siswa_eskul_edit.php
@@ -32,7 +32,7 @@ if (isset($_POST['update'])) {
     <nav class="navbar navbar-dark bg-primary">
         <div class="container-fluid">
             <span class="navbar-brand fw-bold">
-                <i class="bi bi-person-lines-fill"></i> Edit Data Guru
+                <i class="bi bi-person-lines-fill"></i> Edit Data Siswa Eskul
             </span>
         </div>
     </nav>
