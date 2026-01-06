5904e6e92143fdfbfde7214c0968c83e46caf870
Muhammad Miftahul Muhtadin <adinrahman22@gmail.com>
2026-01-06 23:05:52 +0700

    Update eskul_index.php

--- admin/eskul_index.php
+++ admin/eskul_index.php
@@ -45,7 +45,6 @@ if (!$eskul) {
 
     <!-- CONTENT -->
     <div class="container mt-4">
-
     <div class="d-flex justify-content-between align-items-center mb-3">
         <div>
             <h3 class="mb-0">Data Ekstrakurikuler</h3>
